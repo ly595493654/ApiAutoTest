3e65e030b61a720fe3021f3cac2fa9c4787086ee
ly595493654 <75718368+ly595493654@users.noreply.github.com>
2020-12-09 19:46:32 +0800

    Update conftest.py

--- ZongHe/test_script/conftest.py
+++ ZongHe/test_script/conftest.py
@@ -1,9 +1,28 @@
 '''
 脚本层的一些公共方法
 '''
+import os
+import sys
 
 import pytest
 
+def get_project_path():
+    '''
+    获取工程路径
+    :return: 当前工程路径，比如E:\ApiAutoTest\ZongHe\
+    '''
+    # __file__ 存储着当前文件的路径
+    path = os.path.realpath(__file__)
+    # 上一级目录
+    path = os.path.dirname(path)
+    # 再上一级目录
+    path = os.path.dirname(path)
+    path = os.path.dirname(path)
+    return path + "\\"
+
+# python包的搜索路径:当前路径、python安装路径、当前工程路径
+print(get_project_path())
+sys.path.append(get_project_path())
 from ZongHe.caw import DataRead
 from ZongHe.caw.BaseRequests import BaseRequests
 
